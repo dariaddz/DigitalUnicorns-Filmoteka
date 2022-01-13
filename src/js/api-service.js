0ca167a08bdb81b07a9e9b1ef6db99356dafd083
Sergius Nahnoinyi <sergiusnahnoinyi@gmail.com>
2022-01-13 22:52:11 +0200

    Парсит запрос от бекенда

--- src/js/api-service.js
+++ src/js/api-service.js
@@ -1,1 +1,16 @@
 const axios = require('axios');
+
+const API_KEY = '1aaaa4b4eb79ea073919ef453434f2ea';
+const BASE_URL = 'https://api.themoviedb.org/3/';
+
+getFilms();
+
+async function getFilms() {
+  const url = `${BASE_URL}trending/movie/week?api_key=${API_KEY}`;
+  try {
+    const response = await axios.get(url);
+    console.log(response);
+  } catch (error) {
+    console.error(error);
+  }
+}
