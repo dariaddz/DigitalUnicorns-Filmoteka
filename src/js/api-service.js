89ab62c919f26df32be5cc79cc0d5ad9d44de706
Sergius Nahnoinyi <sergiusnahnoinyi@gmail.com>
2022-01-14 19:03:37 +0200

    Рендерит разметку

--- src/js/api-service.js
+++ src/js/api-service.js
@@ -1,16 +1,26 @@
+import filmsTemplate from '../templates/films-list.hbs';
+
 const axios = require('axios');
 
 const API_KEY = '1aaaa4b4eb79ea073919ef453434f2ea';
 const BASE_URL = 'https://api.themoviedb.org/3/';
 
-getFilms();
+const filmsList = document.querySelector('.films-list');
+
+getFilms().then(data => renderFilmsList(data));
 
 async function getFilms() {
   const url = `${BASE_URL}trending/movie/week?api_key=${API_KEY}`;
   try {
     const response = await axios.get(url);
-    console.log(response);
+    const data = response.data.results;
+    console.log(data);
+    return data;
   } catch (error) {
     console.error(error);
   }
+}
+
+function renderFilmsList(data) {
+  filmsList.insertAdjacentHTML('beforeend', filmsTemplate(data));
 }
