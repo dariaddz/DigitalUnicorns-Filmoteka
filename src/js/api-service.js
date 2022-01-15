25b7df86b6a1a0aaff716f23290ba6b7488f6f4c
Sergius Nahnoinyi <sergiusnahnoinyi@gmail.com>
2022-01-15 12:52:03 +0200

    Реализует функцию замены даты в обьектах

--- src/js/api-service.js
+++ src/js/api-service.js
@@ -7,20 +7,36 @@ const BASE_URL = 'https://api.themoviedb.org/3/';
 
 const moviesList = document.querySelector('.movies-list');
 
-getTrendingMovies().then(data => renderTrendingMovies(data));
+getTrendingMovies().then(results => {
+  changeReleaseDate(results);
+  renderTrendingMovies(results);
+});
 
 async function getTrendingMovies() {
   const url = `${BASE_URL}trending/movie/week?api_key=${API_KEY}`;
   try {
     const response = await axios.get(url);
-    const data = response.data.results;
-    console.log(data);
-    return data;
+    const results = response.data.results;
+    console.log(results);
+    return results;
   } catch (error) {
     console.error(error);
   }
 }
 
-function renderTrendingMovies(data) {
-  moviesList.insertAdjacentHTML('beforeend', moviesTemplate(data));
+function renderTrendingMovies(results) {
+  moviesList.insertAdjacentHTML('beforeend', moviesTemplate(results));
+}
+
+function changeReleaseDate(results) {
+  for (let result of results) {
+    let newDate = result.release_date.slice(0, 4);
+
+    Object.defineProperties(result, {
+      release_date: {
+        value: newDate,
+        writable: true,
+      },
+    });
+  }
 }
