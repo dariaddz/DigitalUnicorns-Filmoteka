9e0b438534e924364e1441fb95e2daca2297a2de
dariaddz <dariadz@gmail.com>
2022-01-22 12:47:21 +0200

    js для кнопок библиотеки/форма поиска
    - добавила два рефа - на кнопки и на див с формой поиска
    - написала 4 функции: прячем/показываем кнопки/форму поиска (с 81 строчки)
    - подключила эти функции в функции toHome & toLibrary
    - в scss немного поправила стили по этим рефам

--- src/js/page-header.js
+++ src/js/page-header.js
@@ -1,20 +1,21 @@
-
-
 const refs = {
   headerRef: document.querySelector('header'),
   logoRef: document.querySelector('#logo'),
   homeRef: document.querySelector('#home'),
   libraryRef: document.querySelector('#library'),
 
+  // кнопки library
+  libraryBtns: document.querySelectorAll('.library-button'),
+  // div поиска
+  searchForm: document.querySelector('.search'),
 };
-
 
 refs.logoRef.addEventListener('click', toHome);
 refs.homeRef.addEventListener('click', toHome);
 refs.libraryRef.addEventListener('click', toLibrary);
 
 function toHome(e) {
- e.preventDefault()
+  e.preventDefault();
   const lib = refs.headerRef.classList.contains('page-my-library');
   const det = refs.headerRef.classList.contains('page-details');
 
@@ -28,10 +29,13 @@ function toHome(e) {
   }
 
   refs.homeRef.classList.add('current');
+  // даша добавила функцию
+  libraryBtnsHide();
+  searchFormShow();
 }
 
 function toLibrary(e) {
-  e.preventDefault()
+  e.preventDefault();
   const det = refs.headerRef.classList.contains('page-details');
 
   if (det) {
@@ -41,7 +45,9 @@ function toLibrary(e) {
   refs.homeRef.classList.remove('current');
   refs.headerRef.classList.add('page-my-library');
   refs.libraryRef.classList.add('current');
-
+  // даша добавила функцию
+  libraryBtnsShow();
+  searchFormHide();
 }
 
 function toDetails() {
@@ -56,24 +62,37 @@ function toDetails() {
   refs.headerRef.classList.add('page-details');
 }
 export default function isResults() {
-  const w = JSON.parse(localStorage.getItem('watched')); 
+  const w = JSON.parse(localStorage.getItem('watched'));
   const q = JSON.parse(localStorage.getItem('queue'));
 
-
-  if (w === null || w.length === 0 ) {
+  if (w === null || w.length === 0) {
     refs.noResults.classList.add('visible');
-    
   } else {
-    
     refs.noResults.classList.remove('visible');
   }
 
   if (q === null || q.length === 0) {
-    
     refs.noResults.classList.add('visible');
   } else {
     refs.noResults.classList.remove('visible');
   }
 }
 
+// прячем/показываем  кнопки watched/queue
 
+function libraryBtnsHide() {
+  refs.libraryBtns.forEach(btn => btn.classList.add('is-hidden'));
+}
+
+function libraryBtnsShow() {
+  refs.libraryBtns.forEach(btn => btn.classList.remove('is-hidden'));
+}
+
+// прячем/показываем  форму поиска
+function searchFormHide() {
+  refs.searchForm.classList.add('is-hidden');
+}
+
+function searchFormShow() {
+  refs.searchForm.classList.remove('is-hidden');
+}
