2f1e83262ee6d6fa900c3bf8de6c2bc1db2f0ece
dariaddz <dariadz@gmail.com>
2022-01-25 14:05:12 +0200

    убрала лишние файлы

--- src/index.js
+++ src/index.js
@@ -2,14 +2,9 @@ import './sass/main.scss';
 import './js/page-header';
 import './js/modal';
 import './js/spinner';
-
 import './js/api-service';
 import './js/pagination';
 import './js/rendering';
-
-import './js/authorization';
-
 import './js/back-to-top';
 import './js/one-movie-modal';
-
 import './js/library';
